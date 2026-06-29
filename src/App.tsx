ed9d8a78e1f865b256b4676669b77bdd023c16d8
Paula Ramiro Olivas <paularamiro11@gmail.com>
2026-06-29 13:02:06 +0200

    feature: hero

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import './App.scss'
 import MainLeftbar from './components/main-leftbar/main-leftbar'
+import Home from './views/home/home'
 
 function App() {
 
   return (
     <>
       <MainLeftbar/>
+      <Home/>
     </>
   )
 }
